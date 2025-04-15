72fbfeb8ace43237e9e6e8c847987b4e0b6043cb
andypon <andy.a1204@gmail.com>
2025-04-15 12:24:43 +0800

    初次提交

---
+++ progA_1.c
@@ -0,0 +1,10 @@
+/* progA_1, my first C programing */
+#include <stdio.h> 
+#include <stdlib.h> /* KEEP MS-DOS WINDOWS RESULT */
+int main(void)
+{
+    printf("Hello, C!\n");
+    printf("Hello, World!\n");
+    system("pause"); /* KEEP MS-DOS WINDOWS RESULT */
+    return 0;
+} 
