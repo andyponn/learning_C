e539be0e4a5f24517eb1864ebeee350bc59a7d3a
andypon <andy.a1204@gmail.com>
2025-04-17 22:13:21 +0800

    chapter 3 新建

---
+++ Chapter 3_基本資料型態/prog3_1.c
@@ -0,0 +1,14 @@
+/* prog3_1.c */
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int num1=12400;
+	double num2=5.234;
+	
+	printf("%d is an integer\n", num1);
+	printf("%f is a double\n", num2);
+	
+    system("pause");
+    return 0;
+}
