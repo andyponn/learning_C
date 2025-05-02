20ec5771f84ba0d2b6b809bc556669071cceeeb1
andypon <andy.a1204@gmail.com>
2025-05-03 00:05:17 +0800

    4.3.2 finished

---
+++ Chapter 4_格式化的輸出與輸入/prog4_18.c
@@ -0,0 +1,23 @@
+/* prog4_18, 讀取到錯誤的字元*/
+# include <stdio.h>
+# include <stdlib.h>
+#pragma execution_character_set("utf-8")
+int main(void)
+{   
+	system("chcp 65001 > nul");  // 切換 console 編碼為 UTF-8
+	
+	int num;
+	char ch;
+	
+    printf("請輸入一個整數:"); 
+	scanf("%d", &num);      	
+	
+	printf("請輸入一個字元:"); 
+	scanf("%c", &ch);      	
+	printf("num=%d, ASCII of ch=%d\n",num,ch);
+		
+	system("pause");
+	return 0;
+
+}
+
