7cef5398f3c5e2fb2b2acc3567e5adfd9d9904e4
andypon <andy.a1204@gmail.com>
2025-05-11 23:52:31 +0800

    hw5.1 commit

---
+++ Chapter 5_運算子、運算是與敘述/hw/hw5-1.c
@@ -0,0 +1,12 @@
+/* hw5-1a.c 運算式的練習 */
+#include <stdio.h> 
+#include <stdlib.h>  
+int main(void)
+{
+	int a=8;
+	printf("a=%d\n",++a);
+    printf("a=%d\n",a--);
+   
+    system("pause");  
+    return 0;
+} 
---
+++ Chapter 5_運算子、運算是與敘述/hw/hw5-1b.c
@@ -0,0 +1,15 @@
+/* hw5-1b.c 運算式的練習2 */
+#include <stdio.h> 
+#include <stdlib.h>  
+int main(void)
+{
+	int a=10, b=20;
+	a=a%5;
+	b=b/6;
+	
+	printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    
+    system("pause");  
+    return 0;
+} 
