9dddffcb50cb93bc76e6268a7989e20c8bdf8f2d
andypon <andy.a1204@gmail.com>
2025-05-12 23:27:45 +0800

    hw5.1.2 commit

---
+++ Chapter 5_運算子、運算是與敘述/hw/hw5-2.c
@@ -0,0 +1,14 @@
+/* hw5-2.c True or false */
+#include <stdio.h> 
+#include <stdlib.h>  
+int main(void)
+{	 
+	printf("Qa:%d\n",'a'<28);
+    printf("Qb:%d\n",4+3==8-1);
+    printf("Qc:%d\n",8>2);
+    printf("Qd:%d\n",'a'!=97);
+    
+    
+    system("pause");  
+    return 0;
+} 
